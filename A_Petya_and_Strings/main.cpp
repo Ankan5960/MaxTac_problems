d15789ac94b4f98d040b3953c3635e88b2c6cc00
ankan5960 <ankanmaity27@gmail.com>
2024-03-26 01:45:27 +0530

    A_Petya_and_Strings

---
+++ A_Petya_and_Strings/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str1,str2;
+    cin>>str1>>str2;
+    if(str1>str2)
+        cout<<"1";
+    else if(str1<str2)
+        cout<<"-1";
+    else    
+        cout<<"0";
+}
